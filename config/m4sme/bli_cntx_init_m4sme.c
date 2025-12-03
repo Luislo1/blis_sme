6764898b8cb4c74709a931abf6bb7a4ecbeca563
Luislo1 <luislo11@ucm.es>
2025-12-03 12:30:06 +0100

    Changed block size

--- config/m4sme/bli_cntx_init_m4sme.c
+++ config/m4sme/bli_cntx_init_m4sme.c
@@ -72,8 +72,8 @@ void bli_cntx_init_m4sme( cntx_t* cntx )
 	//                                           s      d      c      z
 	bli_blksz_init_easy( &blkszs[ BLIS_MR ],    32,    32,    -1,    -1 );
 	bli_blksz_init_easy( &blkszs[ BLIS_NR ],    32,    16,    -1,    -1 );
-	bli_blksz_init_easy( &blkszs[ BLIS_MC ],   1280 /*1536 1280 160 or 640 good also*/,   1280,    -1,    -1 );
-	bli_blksz_init_easy( &blkszs[ BLIS_KC ],   1280 /*1536 1280  640*/,   1280,    -1,    -1 );
+	bli_blksz_init_easy( &blkszs[ BLIS_MC ],   1280 /*1536 1280 160 or 640 good also*/,   1024,    -1,    -1 );
+	bli_blksz_init_easy( &blkszs[ BLIS_KC ],   1280 /*1536 1280  640*/,   1024,    -1,    -1 );
 	bli_blksz_init_easy( &blkszs[ BLIS_NC ], 10240,  3072,    -1,    -1 );
 
 	// Update the context with the current architecture's register and cache
